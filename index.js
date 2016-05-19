eb5ff21fa1f4a7ae088f722e7cff959c330cbb2d
pbhadauria2000 <pbhadauria@hotmail.com>
2016-05-19 06:43:17 -0700

    adding basic audit logs

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var auditLogger;
+var defaultLogger = require('./lib/defaultLogger');
+var loopbackHook = require('./lib/loopbackHook');
+
+module.exports = function(app, config) {
+
+    app = app || defaultLogger;
+    var hook;
+
+    // check if the app instance is loopback
+    if(app.hasOwnProperty('loopback')){
+
+        if(!auditLogger) {
+            console.log('WARN: Logger not initialized correctly');
+            auditLogger = defaultLogger;
+        }
+
+        loopbackHook.init(app,config, auditLogger);
+
+        return;
+    } else {
+      auditLogger = app;
+      return auditLogger;
+    }
+};
---
+++ test/index-test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.assert;
+var app = require('./loggerTest');
+var supertest = require('supertest');
+var api = supertest(app);
+
+describe('server', function () {
+
+    var serverInstance;
+
+    //check if server response correctly
+    describe('Test Server status ', function () {
+        it('should respond with 200 status code', function (done) {
+            api.get('/')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    //check if server response correctly
+    describe('Test Log for REST CALL', function () {
+        it('should respond with 200 status code', function (done) {
+            api.get('/api/weather/getWeatherData?city=london')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+});
